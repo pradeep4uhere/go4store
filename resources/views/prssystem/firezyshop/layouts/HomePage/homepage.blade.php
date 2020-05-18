77aa7a24123b16f6d9dec36b4d33333621d6d35c
Pradeep Kumar <kumar031984@gmail.com>
2020-05-19 01:34:06 +0530

    Js and css minify

--- resources/views/prssystem/firezyshop/layouts/HomePage/homepage.blade.php
+++ resources/views/prssystem/firezyshop/layouts/HomePage/homepage.blade.php
@@ -2,9 +2,10 @@
 <html lang="en" class="">
 <head>
 @include('prssystem.firezyshop.layouts.HomePage.homePageMetaHead')
-
 <!-- Templatemela added -->
+<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,400i,600,600i,700,700i,800,800i" rel="stylesheet"> 
+<link href="https://fonts.googleapis.com/css?family=Poppins:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet"> 
 
 <link rel="stylesheet" href="{{config('global.THEME_FRONT_CSS').'/go4shop.online.css'}}">
 </head>
